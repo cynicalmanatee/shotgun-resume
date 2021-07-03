07b39f8dbe916b9067efaad3441669baf947a711
Samuel Tjahjadi <stjahjadi@my.bcit.ca>
2021-07-03 13:24:31 -0700

    fixed resume floating

--- Assets/Gun.cs
+++ Assets/Gun.cs
@@ -4,15 +4,21 @@ public class Gun : MonoBehaviour
 {
     public float damage = 10f;
     public float range = 100f;
+    public float fireRate = 2f;
+    public float impactForce = 10f;
 
     public Camera fpsCam;
     public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
+
+    public float nextFire = 0.0f;
 
     // Update is called once per frame
     void Update() {
 
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1") && Time.time > nextFire)
         {
+            nextFire = Time.time + fireRate;
             Shoot();
         }
     }
@@ -26,10 +32,20 @@ public class Gun : MonoBehaviour
             Debug.Log(hit.transform.name);
 
             Target target = hit.transform.GetComponent<Target>();
+
             if (target != null)
             {
                 target.TakeDamage(damage);
             }
+
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+
+            GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 50f);
         }
     }
 }
+
