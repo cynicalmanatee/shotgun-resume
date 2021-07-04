663cf10904655ee5b3bacbc2976374def6e57299
fma29 <fma29@sfu.ca>
2021-07-04 14:43:25 -0700

    mob spawner now spawns periodically

--- Assets/Script/spawnMob.cs
+++ Assets/Script/spawnMob.cs
@@ -6,21 +6,33 @@ public class spawnMob : MonoBehaviour
 {
 
     public GameObject[]  PrefabtoSpawn;
+
+    public float spawnInterval = 2f;
+
+    public float spawnTime;
+
+    public bool isSpawn;
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
-        spawn();
+        spawnTime = spawnInterval;
     }
 
 
-    void spawn()
+    public void spawn()
     {
-        int x = Random.Range(0,PrefabtoSpawn.Length-1);
-        Instantiate(PrefabtoSpawn[x], transform.position, Quaternion.identity);
+        spawnInterval -= Time.deltaTime;
+        Debug.Log(spawnInterval);
+        if (spawnInterval <= 0 && isSpawn)
+        {
+            int x = Random.Range(0,PrefabtoSpawn.Length-1);
+            Instantiate(PrefabtoSpawn[x], transform.position, Quaternion.identity);
+            spawnInterval = spawnTime;
+        }
     }
     // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        
+        spawn();
     }
 }
