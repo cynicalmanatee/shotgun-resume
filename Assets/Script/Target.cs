c5a7f045520c6f3017a0aecc64dfc3b0fbbd9b77
Harry He <harry.he.zz@gmail.com>
2021-07-04 22:23:27 -0700

    boss percent

--- Assets/Script/Target.cs
+++ Assets/Script/Target.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Target : MonoBehaviour {
-
+public class Target : MonoBehaviour
+{
     public float health = 50f;
 
-    public void TakeDamage (float amount)
+    public float bossPercent = 0.5f;
+
+    public void TakeDamage(float amount)
     {
         health -= amount;
         if (health <= 0f)
@@ -15,6 +18,11 @@ public class Target : MonoBehaviour {
 
     void Die()
     {
-        Destroy(gameObject);
+        if (Random.Range(0f, 100f) == bossPercent)
+        {
+            SceneManager
+                .LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        Destroy (gameObject);
     }
 }
